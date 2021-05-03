54b58e88613687f2189a4282e58bbe140f3a195c
syerlinadya <syerlitrinadya14@gmail.com>
2021-05-03 12:01:10 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'https://notes-app-web-backend.herokuapp.com/',
         routes: {
             cors: {
                 origin: ['*'],
